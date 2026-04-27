24972ebe7fa800289a456823e04c7be68a8ce4e4
Ryan Hoffman <phillykingkong@gmail.com>
2026-04-27 02:50:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7316962736-db703",
+  "appId": "1:827648217878:web:69aad34826071dac4d3637",
+  "apiKey": "AIzaSyAPX5ZtkMmAHIKsKvBemLKdHXpQGHweGkM",
+  "authDomain": "studio-7316962736-db703.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "827648217878"
+};
