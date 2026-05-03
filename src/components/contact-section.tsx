09cf211f8c3deeb1cef43483baef959f8754342a
Ryan Hoffman <phillykingkong@gmail.com>
2026-05-03 12:09:37 +0000

    Remove the map completely (_for element div_)

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -176,18 +176,6 @@ export function ContactSection() {
                 </div>
               </div>
             </div>
-
-            {/* Map Placeholder */}
-            <div className="w-full aspect-video bg-black border border-white/5 overflow-hidden grayscale opacity-50 hover:grayscale-0 hover:opacity-100 transition-all duration-700">
-               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110948.336154562!2d-95.4623!3d29.7604!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640b8b4488d8501%3A0xca0d02def365053b!2sHouston%2C%20TX!5e0!3m2!1sen!2sus!4v1710312000000!5m2!1sen!2sus" 
-                width="100%" 
-                height="100%" 
-                style={{ border: 0 }} 
-                allowFullScreen={true} 
-                loading="lazy" 
-              />
-            </div>
           </div>
 
           {/* Right: Form */}
